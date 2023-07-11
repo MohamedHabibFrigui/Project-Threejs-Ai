05e763ec7ce678be8b5e0c7d7593085930febf37
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2023-07-11 10:30:51 +0100

    Add the Home page

---
+++ client/src/pages/Customizer.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapchat } from "valtio";
+
+import config from "../config/config";
+import state from "../store";
+import { download } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion";
+
+const Customizer = () => {
+  return <div>Customizer</div>;
+};
+
+export default Customizer;
